7c65469d51034f37c5e5fa6f5e4ad4dafed7707d
Harsh8089 <harshjain3575@gmail.com>
2024-10-12 09:13:47 +0530

    added home route for better nav

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    return res.json({
+        "bitcoin": {
+            "/stats": "https://koinx-0jk4.onrender.com/api/v1/stats/bitcoin",
+            "/deviation": "https://koinx-0jk4.onrender.com/api/v1/deviation/bitcoin"
+        },
+        "ethereum": {
+            "/stats": "https://koinx-0jk4.onrender.com/api/v1/stats/ethereum",
+            "/deviation": "https://koinx-0jk4.onrender.com/api/v1/deviation/ethereum"
+        },
+        "matic-network": {
+            "/stats": "https://koinx-0jk4.onrender.com/api/v1/stats/matic-network",
+            "/deviation": "https://koinx-0jk4.onrender.com/api/v1/deviation/matic-network"
+        }
+        
+    })
+});
+
 // Task-1
 import { saveCryptoData } from './controllers/cryptoPriceBackground.controller.js';
 setInterval(saveCryptoData, 2*60*60*1000); // saveCryptoData will be called at every 2 hr gap.
